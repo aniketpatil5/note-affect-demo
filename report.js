a103ee9a94c2e1431568552c387075308effb97b
vrhythms-git <65596801+vrhythms-git@users.noreply.github.com>
2020-09-22 17:01:29 +0530

    Add files via upload
    Committing updated home.html and report.js files to showcase user context on the page.

--- report.js
+++ report.js
@@ -1,50 +1,50 @@
 $(document).ready(function(){
+    debugger;
+    //button for get call
 
-//button for get call
-
-
-setInterval(()=>{
-
- $.get("http://localhost:3000/isAlive", function(data, status){
-	  console.log('is appAlive : ' + data);
-	  if(data == true){
-					document.getElementById("alive-status").style.color = 'green'
-					document.getElementById("alive-status").innerHTML = "ON"  
-					
-					document.getElementById("monitoring-status").style.color = 'green'
-					document.getElementById("monitoring-status").innerHTML= 'ON'
-	  }
-  }).done(function() {
-	  
-			  $.get("http://localhost:3000/getCapturedData", function(data, status){
-			console.log('http://localhost:3000/getCapturedData called with status '+ status);
-			console.log('Data::' + data);
-	 eventList = data["events"];
-	 eventList.forEach(function(obj){ 
-			$('#report-table-body')
-				.append( 
-					'<tr>' +
-						'<td>' + obj["eventType"] + '</td>' +
-						'<td>' + obj["eventMethod"] + '</td>' +
-						'<td>' + obj["dateTime"] + '</td>' +
-						'<td>' + obj["fileName"] + '</td>' +
-						'<td>' + obj["path"] + '</td>' +
-					'</tr>' );
-					
-					//to setup scroll to the bottom
-					var objDiv = document.getElementById("report-table-div");
-					objDiv.scrollTop = objDiv.scrollHeight;
-					
-	 });
-  });
-  }).fail(function() {
-					document.getElementById("alive-status").style.color = 'red'
-					document.getElementById("alive-status").innerHTML= 'OFF'
-					
-					document.getElementById("monitoring-status").style.color = 'red'
-					document.getElementById("monitoring-status").innerHTML= 'OFF'
-	});
-	
-}, 1000);
- 
-});
+    setInterval(()=>{
+    
+     $.get("http://localhost:3000/isAlive", function(data, status){
+          console.log('is appAlive : ' + data);
+          if(data == true){
+                        document.getElementById("alive-status").style.color = 'green'
+                        document.getElementById("alive-status").innerHTML = "ON"  
+                        
+                        document.getElementById("monitoring-status").style.color = 'green'
+                        document.getElementById("monitoring-status").innerHTML= 'ON'
+          }
+      }).done(function() {
+          
+                  $.get("http://localhost:3000/getCapturedData", function(data, status){
+                console.log('http://localhost:3000/getCapturedData called with status '+ status);
+                console.log('Data::' + data);
+         eventList = data["events"];
+         eventList.forEach(function(obj){ 
+                $('#report-table-body')
+                    .append( 
+                        '<tr>' +
+                            '<td>' + obj["eventType"] + '</td>' +
+                            '<td>' + obj["eventMethod"] + '</td>' +
+                            '<td>' + obj["dateTime"] + '</td>' +
+                            '<td>' + obj["fileName"] + '</td>' +
+                            '<td>' + obj["path"] + '</td>' +
+                            '<td>' + obj["userDet"] + '</td>' +
+                            '<td>' + obj["contextDet"] + '</td>' +
+                        '</tr>' );
+                        
+                        //to setup scroll to the bottom
+                        var objDiv = document.getElementById("report-table-div");
+                        objDiv.scrollTop = objDiv.scrollHeight;
+                        
+         });
+      });
+      }).fail(function() {
+                        document.getElementById("alive-status").style.color = 'red'
+                        document.getElementById("alive-status").innerHTML= 'OFF'
+                        
+                        document.getElementById("monitoring-status").style.color = 'red'
+                        document.getElementById("monitoring-status").innerHTML= 'OFF'
+        });
+        
+    }, 1000);
+    });
