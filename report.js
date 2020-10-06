baca33e1438998a799680d80d7902d2c926b0631
vrhythms-git <65596801+vrhythms-git@users.noreply.github.com>
2020-10-06 15:04:35 +0530

    Updated home.html and report.js files
    New files for new user context which has items and security app started flag.

--- report.js
+++ report.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
     debugger;
     //button for get call
-
-    setInterval(()=>{
-    
-     $.get("http://localhost:3000/ping", function(data, status){
-          if(data == true){
-                        document.getElementById("alive-status").style.color = 'green'
-                        document.getElementById("alive-status").innerHTML = "ON"  
-                        
-                        document.getElementById("monitoring-status").style.color = 'green'
-                        document.getElementById("monitoring-status").innerHTML= 'ON'
-          }
-      }).done(function() {
-          
-                  $.get("http://localhost:3000/getCapturedData", function(data, status){
-                console.log('http://localhost:3000/getCapturedData called with status '+ status);
-                console.log('Data::' + data);
-         eventList = data["events"];
-         eventList.forEach(function(obj){ 
+    let isAppInvoked = 0;
+    setInterval(()=>{    
+        $.get("http://localhost:3000/ping", function(data, status){
+            if(data == true){
+                document.getElementById("alive-status").style.color = 'green'
+                document.getElementById("alive-status").innerHTML = "ON"  
+                
+                document.getElementById("monitoring-status").style.color = 'green'
+                document.getElementById("monitoring-status").innerHTML= 'ON'
+            }
+        }).done(function() {
+            console.log("isAppInvoked " + isAppInvoked)
+            if (isAppInvoked == 0){
+                notifyVisibilityChangeToNativeApp(true)
+                isAppInvoked = 1
+            }
+            $.get("http://localhost:3000/getCapturedData", function(data, status){
+            console.log('http://localhost:3000/getCapturedData called with status '+ status);
+            console.log('Data::' + data);
+            eventList = data["events"];
+            eventList.forEach(function(obj){
                 $('#report-table-body')
                     .append( 
                         '<tr>' +
@@ -29,24 +32,21 @@ $(document).ready(function(){
                             '<td>' + obj["path"] + '</td>' +
                             '<td>' + obj["userDet"] + '</td>' +
                             '<td>' + 'seriesId : ' + obj.contextDet.seriesId +', ' 
-									+ 'lectureId : ' + obj.contextDet.lectureId +', '
-									+ 'slide : ' + obj.contextDet.slide +
-									'</td>' +
-                        '</tr>' );
-                        
+                                    + 'lectureId : ' + obj.contextDet.lectureId +', '
+                                    + 'slide : ' + obj.contextDet.slide +
+                                    '</td>' +
+                        '</tr>' );                    
                         //to setup scroll to the bottom
                         var objDiv = document.getElementById("report-table-div");
                         objDiv.scrollTop = objDiv.scrollHeight;
-                        
-         });
-      });
-      }).fail(function() {
-                        document.getElementById("alive-status").style.color = 'red'
-                        document.getElementById("alive-status").innerHTML= 'OFF'
-                        
-                        document.getElementById("monitoring-status").style.color = 'red'
-                        document.getElementById("monitoring-status").innerHTML= 'OFF'
+            });                
         });
-        
+        }).fail(function() {
+            document.getElementById("alive-status").style.color = 'red'
+            document.getElementById("alive-status").innerHTML= 'OFF'
+            
+            document.getElementById("monitoring-status").style.color = 'red'
+            document.getElementById("monitoring-status").innerHTML= 'OFF'
+        });        
     }, 1000);
-    });
+});
