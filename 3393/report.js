bab45786d7a1bcb6519c7dbbb9f9682faa444275
vrhythms-git <65596801+vrhythms-git@users.noreply.github.com>
2020-10-13 20:58:51 +0530

    Added home.html and report.js with new server port config

---
+++ 3393/report.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	try{
+		fetchJwtToken();
+	}catch(error){
+		console.log(error);
+	}
+    //button for get call
+    let isAppInvoked = 0;
+    setInterval(()=>{    
+        $.get("http://localhost:3393/ping", function(data, status){
+			
+			console.log('Ping response : ' + JSON.stringify(data));
+				
+                document.getElementById("alive-status").style.color = 'green'
+                document.getElementById("alive-status").innerHTML = "ON"  
+                
+                document.getElementById("monitoring-status").style.color = 'green'
+                document.getElementById("monitoring-status").innerHTML= 'ON'
+            
+        }).done(function() {
+            console.log("isAppInvoked " + isAppInvoked)
+            if (isAppInvoked == 0){
+                notifyVisibilityChangeToNativeApp(true)
+                isAppInvoked = 1
+            }
+            $.get("http://localhost:3393/getCapturedData", function(data, status){
+            console.log('http://localhost:3393/getCapturedData called with status '+ status);
+            eventList = data["events"];
+            eventList.forEach(function(obj){
+                $('#report-table-body')
+                    .append( 
+                        '<tr>' +
+                            '<td>' + obj["eventType"] + '</td>' +
+                            '<td>' + obj["eventMethod"] + '</td>' +
+                            '<td>' + obj["eventDate"] + '</td>' +
+                            '<td>' + obj["fileName"] + '</td>' +
+                            '<td>' + obj["path"] + '</td>' +
+                            '<td>' + obj["userDet"] + '</td>' +
+                            '<td>' + 'seriesId : '     + obj.contextDet.seriesId +', ' 
+							       + 'resourceType : ' + obj.contextDet.resourceType +', '
+                                   + 'lectureId : '    + obj.contextDet.item.lectureId +', '
+                                   + 'slide : '        + obj.contextDet.item.slide +
+                                    '</td>' +
+						    '<td>' + obj["detail"] + '</td>' +
+                        '</tr>' );                    
+                        //to setup scroll to the bottom
+                        var objDiv = document.getElementById("report-table-div");
+                        objDiv.scrollTop = objDiv.scrollHeight;
+            });                
+        });
+        }).fail(function() {
+            document.getElementById("alive-status").style.color = 'red'
+            document.getElementById("alive-status").innerHTML= 'OFF'
+            
+            document.getElementById("monitoring-status").style.color = 'red'
+            document.getElementById("monitoring-status").innerHTML= 'OFF'
+        });        
+    }, 1000);
+});
